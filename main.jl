f79c7ac2a69e639cbf0042670ff8d8b13eb870b3
Hiroyuki Shindo <hshindo@users.noreply.github.com>
2018-08-02 18:38:06 +0900

    Add no split option

--- main.jl
+++ main.jl
@@ -3,7 +3,8 @@ using TSGs
 config = Dict()
 config["input_file"] = "data/wsj_22.mrg"
 config["output_file"] = "out.txt"
-config["nepochs"] = 10
+config["nepochs"] = 100
+config["nosplit"] = ["_I"]
 
 trainer = Trainer(config)
 gibbs!(trainer)
